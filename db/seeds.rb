0485f06ab1099231c9321650668a0a99f48d3119
Christoph Wong & <pair+christophwong.heaphyg@devbootcamp.com>
2014-04-11 18:46:06 -0500

    Added navigation bar with routing and login to layout.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-# require 'faker'
+require 'faker'
 
-# 4.times do
-#   User.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "123", password_confirmation: "123", image_url: Faker::Internet.url)
-#   Survey.create!(title: Faker::Team.creature, creator_id: rand(1..4), image_url: Faker::Internet.url)
-#   Question.create!(survey_id: rand(1..4), text: Faker::Lorem.words(9).join(' '), image_url: Faker::Internet.url)
-#   Choice.create!(question_id: rand(1..4), option: Faker::Lorem.words(2).join(' '))
-#   Participation.create!(taker_id: rand(1..4), survey_id: rand(1..4))
-#   Answer.create!(participation_id: rand(1..4), choice_id: rand(1..4))
-# end
+4.times do
+  User.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "123", password_confirmation: "123", image_url: Faker::Internet.url)
+  Survey.create!(title: Faker::Team.creature, creator_id: rand(1..4), image_url: Faker::Internet.url)
+  Question.create!(survey_id: rand(1..4), text: Faker::Lorem.words(9).join(' '), image_url: Faker::Internet.url)
+  Choice.create!(question_id: rand(1..4), option: Faker::Lorem.words(2).join(' '))
+  Participation.create!(taker_id: rand(1..4), survey_id: rand(1..4))
+  Answer.create!(participation_id: rand(1..4), choice_id: rand(1..4))
+end
 
 
 
