c7339774b66fa58be1c127c1b0bb52c86cd9f8e7
Christoph Wong <wong.christoph@gmail.com>
2014-04-12 21:55:02 -0500

    image upload works for surveys, realize paths not right, will wait til form is finalize to change display image option

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
+  mount_uploader :filepath, Uploader
   has_many :questions
   has_many :participations
   has_many :takers, through: :participations
